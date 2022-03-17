96379e3b9488768d41ed03536b5568ce697efa4f
kamilquliyev98 <kamilag@code.edu.az>
2022-03-17 13:27:58 +0400

    class for db

---
+++ Nest-Multipurpose-eCommerce/DAL/NestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest_Multipurpose_eCommerce.DAL
+{
+    public class NestDbContext : DbContext
+    {
+        public NestDbContext(DbContextOptions<NestDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
